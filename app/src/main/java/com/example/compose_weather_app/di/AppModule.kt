afef37744cf9709f30e7cb5d6021ca32901a5655
Hung Diep <quoc.hung.diep@outlook.com>
2023-02-28 16:12:41 +0000

    Setup AppModule object for dependency injection

---
+++ app/src/main/java/com/example/compose_weather_app/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.compose_weather_app.di
+
+import com.example.compose_weather_app.common.Constants
+import com.example.compose_weather_app.data.remote.WeatherApi
+import com.example.compose_weather_app.data.repository.WeatherRepositoryImpl
+import com.example.compose_weather_app.domain.repository.WeatherRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(api: WeatherApi): WeatherRepository {
+        return WeatherRepositoryImpl(api)
+    }
+
+}
