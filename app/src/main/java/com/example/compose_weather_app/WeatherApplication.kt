53f2b966f55bdfa4d581c3b6166fb5d5617fa1cb
Hung Diep <quoc.hung.diep@outlook.com>
2023-02-28 16:55:00 +0000

    Add Hilt application class
    See https://developer.android.com/training/dependency-injection/hilt-android
    for more details.

---
+++ app/src/main/java/com/example/compose_weather_app/WeatherApplication.kt
@@ -0,0 +1,8 @@
+package com.example.compose_weather_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application() {
+}
