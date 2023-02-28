5d076a3df1030a86c365d33375a716fa4e955246
Hung Diep <quoc.hung.diep@outlook.com>
2023-02-28 20:21:06 +0000

    Setup MainActivity to display WeatherDisplayScreen
    - Add a background to the screen.
    - Create a navController, with the starting destination as the WeatherDisplayScreen.
    - Create a NavGraphBuilder that contains routes to all screens.

--- app/src/main/java/com/example/compose_weather_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/compose_weather_app/ui/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.compose_weather_app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.compose_weather_app.R
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose_weather_app.ui.presentation.Screen
+import com.example.compose_weather_app.ui.presentation.weather_display.WeatherDisplayScreen
+import com.example.compose_weather_app.ui.theme.Material3Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Material3Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.WeatherDisplayScreen.route
+                    ) {
+                        composable(
+                            route = Screen.WeatherDisplayScreen.route
+                        ) {
+                            WeatherDisplayScreen()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
