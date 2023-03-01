7e5384f6c95a4fb72ec596e0a6df88f480c62cda
Hung Diep <quoc.hung.diep@outlook.com>
2023-03-01 15:34:56 +0000

    Update domain-level class to store data values

--- app/src/main/java/com/example/compose_weather_app/domain/model/WeatherData.kt
+++ app/src/main/java/com/example/compose_weather_app/domain/model/WeatherData.kt
@@ -1,5 +1,17 @@
 package com.example.compose_weather_app.domain.model
 
+import com.example.compose_weather_app.data.remote.dto.Daily
+import com.example.compose_weather_app.data.remote.dto.Hourly
+
 data class WeatherData(
-    val timezone: String
+    val currentTemperature: String,
+    val currentWeatherCode: String,
+    val sunrise: List<String>,
+    val sunset: List<String>,
+    val dailyMaxTemperature: List<Double>,
+    val dailyMinTemperature: List<Double>,
+    val dailyMaxWindSpeed: List<Double>,
+    val dailyWeatherCode: List<Int>,
+    val latitude: Double,
+    val longitude: Double
 )
