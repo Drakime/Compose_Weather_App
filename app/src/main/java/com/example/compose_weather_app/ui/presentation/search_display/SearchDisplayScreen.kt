acae3a721deb7d63233a0ff4a1fcb7082a4f5c89
Hung Diep <quoc.hung.diep@outlook.com>
2023-03-14 20:46:11 +0000

    Implement placeholder search location screen
    TODO: The search screen currently uses a placeholder data class
    inside of the viewmodel. To adapt this to your application,
    you need to have a list of locations that you filter, and return
    as a list. Afterwards, replace all list instances inside the
    viewmodel with the new list. Also, you need to use this information
    to change the API query so that you update the main weather display
    with the new location.

--- app/src/main/java/com/example/compose_weather_app/ui/presentation/search_display/SearchDisplayScreen.kt
+++ app/src/main/java/com/example/compose_weather_app/ui/presentation/search_display/SearchDisplayScreen.kt
@@ -1,14 +1,20 @@
 package com.example.compose_weather_app.ui.presentation.search_display
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
@@ -18,9 +24,9 @@ fun SearchDisplayScreen(
     viewModel: SearchDisplayViewModel = hiltViewModel(),
     navController: NavController
 ) {
-    var showClearButton by remember { mutableStateOf(false) }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val focusRequester = remember { FocusRequester() }
+    val searchText by viewModel.searchText.collectAsState()
+    val cities by viewModel.cities.collectAsState()
+    val isSearching by viewModel.isSearching.collectAsState()
 
     TopAppBar(title = { Text("") }, navigationIcon = {
         IconButton(onClick = { navController.navigateUp() }) {
@@ -32,8 +38,42 @@ fun SearchDisplayScreen(
         }
     }, actions = {
 
-
-
     }
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = searchText,
+            onValueChange = viewModel::onSearchTextChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = "Search") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        if (isSearching) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                items(cities) { city ->
+                    Text(
+                        text = "${city.name}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/compose_weather_app/ui/presentation/search_display/SearchDisplayViewModel.kt
+++ app/src/main/java/com/example/compose_weather_app/ui/presentation/search_display/SearchDisplayViewModel.kt
@@ -1,7 +1,66 @@
 package com.example.compose_weather_app.ui.presentation.search_display
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 
+@OptIn(FlowPreview::class)
 class SearchDisplayViewModel : ViewModel() {
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private val _cities = MutableStateFlow(allCities)
+    val cities = searchText
+        .debounce(500L)
+        .onEach { _isSearching.update { true } }
+        .combine(_cities) { text, cities ->
+            if (text.isBlank()) {
+                cities
+            } else {
+                delay(1000L)
+                cities.filter {
+                    it.doesMatchSearchQuery(text)
+                }
+            }
+        }
+        .onEach { _isSearching.update { false } }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _cities.value
+        )
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
 }
+
+data class City(
+    val name: String
+) {
+    fun doesMatchSearchQuery(query: String): Boolean {
+        val matchingCombinations = listOf(
+            "$name"
+        )
+
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allCities = listOf(
+    City(name = "London"),
+    City(name = "Leeds"),
+    City(name = "Manchester"),
+    City(name = "Brighton"),
+    City(name = "Tokyo"),
+    City(name = "Norwich"),
+)
