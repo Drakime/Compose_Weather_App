edd0b1f23c31421ed5934be4b47f2f74dda36d70
Hung Diep <quoc.hung.diep@outlook.com>
2023-03-14 00:05:42 +0000

    Add App Bar to weather display

--- app/src/main/java/com/example/compose_weather_app/ui/presentation/weather_display/WeatherDisplayScreen.kt
+++ app/src/main/java/com/example/compose_weather_app/ui/presentation/weather_display/WeatherDisplayScreen.kt
@@ -1,20 +1,17 @@
 package com.example.compose_weather_app.ui.presentation.weather_display
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.compose_weather_app.ui.presentation.components.*
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun WeatherDisplayScreen(
     viewModel: WeatherDisplayViewModel = hiltViewModel()
@@ -23,14 +20,23 @@ fun WeatherDisplayScreen(
     val scrollState = rememberScrollState()
 
     state.weather?.let {
-        Column(modifier = Modifier
-            .verticalScroll(state = scrollState)
-            .padding(10.dp)) {
-            WeatherDisplayTemperatureCard(weatherData = state.weather)
-            WeatherDisplayForecastCard(weatherData = state.weather)
-            WeatherDisplayWindCard(weatherData = state.weather)
-            WeatherDisplaySunriseSunsetCard(weatherData = state.weather)
-            WeatherDisplayPrecipitationCard(weatherData = state.weather)
+        Column {
+            Row() {
+                WeatherDisplayAppBar()
+            }
+            Row() {
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(state = scrollState)
+                        .padding(10.dp)
+                ) {
+                    WeatherDisplayTemperatureCard(weatherData = state.weather)
+                    WeatherDisplayForecastCard(weatherData = state.weather)
+                    WeatherDisplayWindCard(weatherData = state.weather)
+                    WeatherDisplaySunriseSunsetCard(weatherData = state.weather)
+                    WeatherDisplayPrecipitationCard(weatherData = state.weather)
+                }
+            }
         }
     }
 }
