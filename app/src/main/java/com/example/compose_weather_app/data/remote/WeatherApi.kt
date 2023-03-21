275719ad3c07aed7a368a8ca005f778b765a870e
Hung Diep <quoc.hung.diep@outlook.com>
2023-03-21 02:21:35 +0000

    Modify WeatherAPi call to use queries

--- app/src/main/java/com/example/compose_weather_app/data/remote/WeatherApi.kt
+++ app/src/main/java/com/example/compose_weather_app/data/remote/WeatherApi.kt
@@ -2,8 +2,15 @@ package com.example.compose_weather_app.data.remote
 
 import com.example.compose_weather_app.data.remote.dto.WeatherDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET("forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon")
-    suspend fun getWeather(): WeatherDto
+    @GET("forecast")
+    suspend fun getWeather(
+        @Query("latitude", encoded = true) latitude: String = "51.51",
+        @Query("longitude", encoded = true) longitude: String = "-0.13",
+        @Query("hourly", encoded = true) hourly: String = "temperature_2m,weathercode",
+        @Query("daily", encoded = true) daily: String = "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max",
+        @Query("timezone", encoded = true) timezone: String = "Europe%2FLondon"
+    ): WeatherDto
 }
