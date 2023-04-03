d30ce95b476feb194d4e5ba88b2cc24b5e46ebb8
Hung Diep <quoc.hung.diep@outlook.com>
2023-04-03 16:53:54 +0100

    Modify CityAPI to use new API

--- app/src/main/java/com/example/compose_weather_app/common/Constants.kt
+++ app/src/main/java/com/example/compose_weather_app/common/Constants.kt
@@ -2,5 +2,6 @@ package com.example.compose_weather_app.common
 
 object Constants {
     const val BASE_URL = "https://api.open-meteo.com/v1/"
-    const val CITIES_BASE_URL = "https://geocode.maps.co/"
+    const val CITIES_BASE_URL = "https://geocoding-api.open-meteo.com/v1/"
+    const val WEATHER_SCREEN_PREFERENCES_DATASTORE_NAME = "weather_screen_preferences"
 }
--- app/src/main/java/com/example/compose_weather_app/data/remote/CityApi.kt
+++ app/src/main/java/com/example/compose_weather_app/data/remote/CityApi.kt
@@ -2,8 +2,12 @@ package com.example.compose_weather_app.data.remote
 
 import com.example.compose_weather_app.data.remote.dto.CityDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CityApi {
-    @GET("search?q={location}")
-    suspend fun getCities(): List<CityDto>
+    @GET("search?")
+    suspend fun getCities(
+        @Query("name") location: String,
+        @Query("count") count: Int = 100
+    ): CityDto
 }
