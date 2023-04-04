cb8a287351e2764f8245144067e584063253793a
Hung Diep <quoc.hung.diep@outlook.com>
2023-04-04 16:19:37 +0100

    Amend encoded queries

--- app/src/main/java/com/example/compose_weather_app/data/remote/WeatherApi.kt
+++ app/src/main/java/com/example/compose_weather_app/data/remote/WeatherApi.kt
@@ -3,14 +3,15 @@ package com.example.compose_weather_app.data.remote
 import com.example.compose_weather_app.data.remote.dto.WeatherDto
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 interface WeatherApi {
     @GET("forecast")
     suspend fun getWeather(
-        @Query("latitude", encoded = true) latitude: String = "51.51",
-        @Query("longitude", encoded = true) longitude: String = "-0.13",
-        @Query("hourly", encoded = true) hourly: String = "temperature_2m,weathercode",
-        @Query("daily", encoded = true) daily: String = "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max",
-        @Query("timezone", encoded = true) timezone: String = "Europe%2FLondon"
+        @Query("latitude") latitude: String = "51.51",
+        @Query("longitude") longitude: String = "-0.13",
+        @Query("hourly") hourly: String = "temperature_2m,weathercode",
+        @Query("daily") daily: String = "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max",
+        @Query("timezone", encoded = true) timezone: String = "Europe%2FLondon" //  You may want to supply a preference option for the user to set timezone.
     ): WeatherDto
 }
